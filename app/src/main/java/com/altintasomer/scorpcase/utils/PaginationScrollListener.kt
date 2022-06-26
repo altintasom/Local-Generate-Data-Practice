27ed48f7f442846d48f6dc65ce2f37bcb72fba58
ömer altıntaş <omer.altintas@morhipo.com>
2022-06-26 18:25:10 +0300

    checking loading when  request in pagination

--- app/src/main/java/com/altintasomer/scorpcase/utils/PaginationScrollListener.kt
+++ app/src/main/java/com/altintasomer/scorpcase/utils/PaginationScrollListener.kt
@@ -8,33 +8,35 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.log
 
 private const val TAG = "PaginationScrollListene"
-abstract class PaginationScrollListener(private val layoutManager: GridLayoutManager) : RecyclerView.OnScrollListener() {
+
+abstract class PaginationScrollListener(private val layoutManager: GridLayoutManager) :
+    RecyclerView.OnScrollListener() {
 
     var isScrolling = false
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
 
 
-
     }
 
     override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
         super.onScrollStateChanged(recyclerView, newState)
-        if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
-           isScrolling = true
+        if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+            isScrolling = true
         }
-        val visibleItemCount  = layoutManager.childCount
+        val visibleItemCount = layoutManager.childCount
         val totalItemCount = layoutManager.itemCount
         val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
 
-        if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0){
-            loadMoreItems()
-        }
+        if (!isLoading())
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0) {
+                loadMoreItems()
+            }
     }
 
 
     abstract fun loadMoreItems()
-    abstract fun isLoading() : Boolean
+    abstract fun isLoading(): Boolean
 
 
 }
